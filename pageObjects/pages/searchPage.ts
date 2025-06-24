68910469f339a8447fca2168b82964945c663b49
wildnyker <sgrynchuk@outlook.com>
2025-06-24 23:08:19 +0200

    search tests skeleton + search page POM skeleton

--- pageObjects/pages/searchPage.ts
+++ pageObjects/pages/searchPage.ts
@@ -2,8 +2,45 @@ import { type Page, type Locator } from "@playwright/test";
 
 export class SearchPage {
   readonly page: Page;
+  readonly searchField: Locator;
+  readonly resultsBox: Locator;
+  readonly resultsFilterDropdown: Locator;
+  readonly zahidDialectCheckbox: Locator;
+  readonly guculDialewctCheckbox: Locator;
+  readonly sloboDialectCheckbox: Locator;
+  readonly applyFilterButton: Locator;
+  readonly youSearchedParagraph: Locator;
+  readonly articleBox: Locator;
+  readonly articleHeading: Locator;
+  readonly articleDialect: Locator;
+  readonly articleFirstDefinition: Locator;
+  readonly articleFirtsDefinitionSource: Locator;
+  readonly articleOtherDictionarySUMLink: Locator;
+  readonly articleOtherDictionaryGrinLink: Locator;
+  readonly articleOtherDictionarySlovMeLink: Locator;
+  readonly articleOtherDictionarySlovUaLink: Locator;
+  readonly articleOtherDictionaryEngLink: Locator;
+
 
   constructor(page: Page) {
     this.page = page;
+    this.searchField = this.page.getByRole('textbox', { name: 'search' })
+    this.resultsBox = this.page.locator('.center')
+    this.resultsFilterDropdown = this.page.getByRole('button', { name: 'Filter' })
+    this.zahidDialectCheckbox = this.page.getByRole('checkbox', { name: 'Західнополіський' })
+    this.guculDialewctCheckbox = this.page.getByRole('checkbox', { name: 'Гуцульський' })
+    this.sloboDialectCheckbox =this.page.getByRole('checkbox', { name: 'Слобожанський' })
+    this.applyFilterButton = this.page.locator('#dialectFilter').getByRole('button', { name: 'Filter' })
+    this.youSearchedParagraph = this.page.getByText('You searched:')
+    this.articleBox = this.page.locator('article')
+    this.articleHeading = this.articleBox.getByRole('heading')
+    this.articleDialect = this.articleBox.locator("p",{hasText:/Filter/})
+    this.articleFirstDefinition = this.articleBox.locator('li').first()
+    this.articleFirtsDefinitionSource = this.articleBox.locator('.small-font')
+    this.articleOtherDictionarySUMLink = this.articleBox.locator('a', {hasText:"СУМ"})
+    this.articleOtherDictionaryGrinLink = this.articleBox.locator('a', {hasText:"Грінченка"})
+    this.articleOtherDictionarySlovMeLink = this.articleBox.locator('a', {hasText:"Slovnyk.me"})
+    this.articleOtherDictionarySlovUaLink = this.articleBox.locator('a', {hasText:"Slovnyk.ua"})
+    this.articleOtherDictionaryEngLink = this.articleBox.locator('a', {hasText:"ENG"})
   }
 }
--- tests/searchTests/searchPageCommonTests.spec.ts
+++ tests/searchTests/searchPageCommonTests.spec.ts
@@ -4,3 +4,27 @@ import { PageManager } from "../../pageObjects/pageManager";
 test.beforeEach(async ({ page }) => {
   page.goto("/search");
 });
+
+
+test.describe('standard search tests',()=>{
+
+  test('submit empty search query', async({page})=>{
+    
+  })
+
+
+  test('submit 1 letter search query', async({page})=>{
+
+  })
+
+
+  test('submit 1 word serch query', async({page})=>{
+
+  })
+
+
+  test('submit a search query with 0 results', async({page})=>{
+
+  })
+
+})
