867d02595b6fa1efd5aa4fac787f87c921850d1b
wildnyker <sgrynchuk@outlook.com>
2025-06-24 23:44:50 +0200

    first search test2

--- pageObjects/pages/aboutPage.ts
+++ pageObjects/pages/aboutPage.ts
@@ -10,7 +10,7 @@ export class AboutPage {
   readonly aboutSourcesElementsList: Locator;
   constructor(page: Page) {
     this.page = page;
-    this.aboutHeader = this.page.getByRole('heading');
+    this.aboutHeader = this.page.getByRole('heading', {name:'Library of Dialects'});
     this.aboutDescription = this.page.getByText(
       "Welcome to the online library"
     );
