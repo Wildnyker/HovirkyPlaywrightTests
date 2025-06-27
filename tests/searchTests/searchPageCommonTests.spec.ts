e09f469866bedd9891d3665b3baa4cd70b893c95
wildnyker <sgrynchuk@outlook.com>
2025-06-27 23:03:37 +0200

    additional search test cases added

--- tests/searchTests/searchPageCommonTests.spec.ts
+++ tests/searchTests/searchPageCommonTests.spec.ts
@@ -6,7 +6,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 
-test.describe('standard search tests',()=>{
+test.describe('standard search tests with all dialects enabled',()=>{
     
   test('submit empty search query', async({page})=>{
     //Arrange: page object
@@ -15,23 +15,45 @@ test.describe('standard search tests',()=>{
     //Act: performing a search with an empty query
     await pm.onSearchPage().performSearch()
 
-    //Assert: full dictionary returned starting from a first entry
+    //Assert: full dictionary returned starting from a first entry 'АБА́ЗЬ'
     await expect(pm.onSearchPage().articleHeading.first()).toHaveText('АБА́ЗЬ')
   })
 
 
   test('submit 1 letter search query', async({page})=>{
+    //Arrange: page object
+    const pm = new PageManager(page)
+    
+    //Act: performing a search with an empty query
+    await pm.onSearchPage().performSearch('ї')
 
+    //Assert: А́ЇСТ is returned as a first entry
+    await expect(pm.onSearchPage().articleHeading.first()).toHaveText('А́ЇСТ') 
   })
 
 
   test('submit 1 word serch query', async({page})=>{
+    //Arrange: page object
+    const pm = new PageManager(page)
+    
+    //Act: performing a search with 'кофта'
+    await pm.onSearchPage().performSearch('кофта')
+
+    //Assert: БА́СКА(other type of кофта) is returned as firts entry
+    await expect(pm.onSearchPage().articleHeading.first()).toHaveText('БА́СКА') 
 
   })
 
 
   test('submit a search query with 0 results', async({page})=>{
+    //Arrange: page object
+    const pm = new PageManager(page)
+    
+    //Act: performing a search with an empty query
+    await pm.onSearchPage().performSearch('rocket')
 
+    //Assert that no article headings are visible on the page
+    await expect(pm.onSearchPage().articleHeading).toHaveCount(0)
   })
 
 })
